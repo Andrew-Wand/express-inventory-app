fd10d8dc6b2d0323885323b27bd016476bcd24f6
Andrew-Wand <andrewwands@gmail.com>
2024-02-21 16:14:04 -0600

    added env

--- app.js
+++ app.js
@@ -13,9 +13,7 @@ const app = express();
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
-const dev_db_url =
-  "mongodb+srv://andrewwands:patricklogan123@inventorydb.uhpnti3.mongodb.net/inventory_db?retryWrites=true&w=majority&appName=InventoryDB";
-const mongoDB = process.env.MONGODB_URI || dev_db_url;
+const mongoDB = process.env.MONGODB_URI;
 
 main().catch((err) => console.log(err));
 async function main() {
