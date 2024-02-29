e3ab8d10cc0368ba53ba3733a8d2e50ee24fbfe9
Andrew-Wand <andrewwands@gmail.com>
2024-02-28 19:25:29 -0600

    can now add item to list

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -3,6 +3,7 @@ const Brand = require("../models/brand");
 const Category = require("../models/category");
 const ItemInstance = require("../models/itemInstance");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 exports.index = asyncHandler(async (req, res, next) => {
   const [numItems, numItemInstances, numBrands, numCategories] =
@@ -32,20 +33,111 @@ exports.item_list = asyncHandler(async (req, res, next) => {
   res.render("item_list", { title: "All Tennis Rackets", item_list: allItems });
 });
 
-// Display detail page for a specific item.
+// Display detail page for a specific book.
 exports.item_detail = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: item detail: ${req.params.id}`);
+  // Get details of books, book instances for specific book
+  const [item, itemInstances] = await Promise.all([
+    Item.findById(req.params.id).populate("category").populate("brand").exec(),
+    ItemInstance.find({ item: req.params.id }).exec(),
+  ]);
+
+  if (item === null) {
+    // No results.
+    const err = new Error("Item not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("item_detail", {
+    title: item.title,
+    item: item,
+    item_instances: itemInstances,
+  });
 });
 
-// Display item create form on GET.
+// Display book create form on GET.
 exports.item_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: item create GET");
+  // Get all authors and genres, which we can use for adding to our book.
+  const [allBrands, allCategories] = await Promise.all([
+    Brand.find().sort({ name: 1 }).exec(),
+    Category.find().sort({ name: 1 }).exec(),
+  ]);
+
+  res.render("item_form", {
+    title: "Create Tennis Item",
+    brands: allBrands,
+    categories: allCategories,
+  });
 });
 
-// Handle item create on POST.
-exports.item_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: item create POST");
-});
+// Handle book create on POST.
+exports.item_create_post = [
+  // Convert the genre to an array.
+  (req, res, next) => {
+    if (!Array.isArray(req.body.genre)) {
+      req.body.genre =
+        typeof req.body.genre === "undefined" ? [] : [req.body.genre];
+    }
+    next();
+  },
+
+  // Validate and sanitize fields.
+  body("title", "Title must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("brand", "Brand must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("description", "Description must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("category.*").escape(),
+  body("price", "Price must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  // Process request after validation and sanitization.
+
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a Book object with escaped and trimmed data.
+    const item = new Item({
+      title: req.body.title,
+      description: req.body.description,
+      brand: req.body.brand,
+      category: req.body.category,
+      price: req.body.price,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/error messages.
+
+      // Get all authors and genres, which we can use for adding to our book.
+      const [allBrands, allCategories] = await Promise.all([
+        Brand.find().sort({ name: 1 }).exec(),
+        Category.find().sort({ name: 1 }).exec(),
+      ]);
+
+      res.render("item_form", {
+        title: "Create Item",
+        brands: allBrands,
+        categories: allCategories,
+        item: item,
+        errors: errors.array(),
+      });
+    } else {
+      // Data from form is valid. Save book.
+      await item.save();
+      res.redirect(item.url);
+    }
+  }),
+];
 
 // Display item delete form on GET.
 exports.item_delete_get = asyncHandler(async (req, res, next) => {
